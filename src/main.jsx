5e67ca0c11ca68b5890f15265315242569d5a934
risalshahed <risalshahed@gmail.com>
2024-01-22 02:54:04 +0600

    project installed

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import '../src/assets/scss/App.scss';
+import { BrowserRouter } from 'react-router-dom';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>
+)
